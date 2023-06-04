5e08c4a4d5ec740771b783103e6a68f1dba3b49c
Mohamed <s2000588@edu.bc.fi>
2023-06-04 20:39:12 +0300

    start the backend

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const data = require("./data/data.json");
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+ 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to my application.",
+    data: data,
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000.");
+});
